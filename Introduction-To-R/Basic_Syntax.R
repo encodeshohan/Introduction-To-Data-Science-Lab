e121dfbcea1ed72a7096139fecd8bf06d38f4f50
Md. Shohanur Rahman Shohan <msrsshohan@gmail.com>
2024-10-27 22:39:57 +0600

    basicSyntax

---
+++ Introduction-To-R/Basic_Syntax.R
@@ -0,0 +1,55 @@
+1+1
+2+3
+4+3
+
+2+3*4
+3^2
+sqrt(10)
+pi
+
+# This is a comment
+
+# djkhfjehgfj
+# jhgdfkujhfgkjhf
+# khjkhgf
+
+x <- 4.25
+y <- 1
+z <- 2
+x * y * z
+
+
+
+abs(x)
+sqrt(x)
+ceiling(x)
+floor(x)
+
+p <- "HELLO WORLD"
+tolower(p)
+q <- "hello world"
+toupper(q)
+strsplit(p, split="O")
+
+n <- "Hello"
+o <- "World"
+nchar(n)
+paste0(n, o)
+gsub(" ", "", paste(n, o))
+
+pp = "jkhfdjg,
+udfhgi,
+djfhgjk,
+hfgukyfg"
+pp
+
+cat(pp)
+
+
+a <- 50
+b <- 10
+if(a<b){
+  print("a is less then b")
+} else{
+  print("a is grater then b")
+}
